eb72aa8f51bcf04f2048bec6734e57be822c7724
Víctor Roberto León Guerra <victorleon.dev@gmail.com>
2025-03-31 18:44:48 +0000

    agregar logo

---
+++ src/js/components/navbar.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import logo from './img/logo_dark.svg';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+      <div className="container-fluid">
+      <a className="navbar-brand" href="/">
+          <img src={logo} alt="Logo" style={{ height: '40px' }} />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="/">
+                Inicio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link">
+                Acerca de
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link">
+                Servicios
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link">
+                Contacto
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
